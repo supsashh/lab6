d0be387b21b443ace08ea1502eb59cfb1d12b2bb
Alexander Goldshteyn <asgold8@yahoo.com>
2016-02-11 20:00:35 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -25,8 +25,18 @@ function addProjectDetails(e) {
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
-
+	$.get("/project/"+idNumber,callback);
 	console.log("User clicked on project " + idNumber);
+}
+
+function callback(result){
+	console.log(result);
+	var projectHTML = '<img src="' + result.image+'" class="detailsImage">'+
+		'<p>' + result.title + '</p>' +
+		'<p><small>' + result.date + '</small></p>' + result.summary;
+
+	$("#project"+result.id+" .details").html(projectHTML);
+	//#project1 > div > div
 }
 
 /*
@@ -35,4 +45,15 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette", colorCall);
+}
+
+function colorCall(result){
+	var colors = result.colors.hex;
+	console.log(colors);
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
